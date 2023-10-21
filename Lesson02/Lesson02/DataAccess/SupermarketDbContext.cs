02f05577eb6e025a0f52bc3f2996cffee18efd11
Miraziz.Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2023-10-21 17:22:41 +0500

    lesson exercises

---
+++ Lesson02/Lesson02/DataAccess/SupermarketDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Lesson02.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lesson02.DataAccess
+{
+    public class SupermarketDbContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+
+        public SupermarketDbContext(DbContextOptions<SupermarketDbContext> options) : base(
+            options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .ToTable("Product");
+            modelBuilder.Entity<Product>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<Product>()
+                .Property(x => x.Name)
+                .HasMaxLength(250)
+                .IsRequired();
+            modelBuilder.Entity<Product>()
+                .Property(x => x.Price)
+                .HasColumnType("money");
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<Category>()
+                .ToTable("Category");
+            modelBuilder.Entity<Category>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<Category>()
+                .Property(x => x.Name)
+                .HasMaxLength(250)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Lesson02/Lesson02/Extensions/DatabaseSeeder.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+using Lesson02.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lesson02.Extensions
+{
+    public class DatabaseSeeder
+    {
+        public static void Initialize(IServiceProvider provider)
+        {
+            var options = provider.GetRequiredService<DbContextOptions<SupermarketDbContext>>();
+            using var context = new SupermarketDbContext(options);
+
+            if (context.Products.Any())
+            {
+                return;
+            }
+
+            var faker = new Faker();
+            string[] categoryNames = faker.Commerce.Categories(25);
+
+            foreach (var categoryName in categoryNames)
+            {
+                context.Categories.Add(new Models.Category()
+                {
+                    Name = categoryName
+                });
+            }
+
+            var categories = context.Categories.ToList();
+
+            foreach (var category in categories)
+            {
+                int randomProductsCount = new Random().Next(5, 20);
+
+                for (int i = 0; i < randomProductsCount; i++)
+                {
+                    var product = new Models.Product()
+                    {
+                        Name = faker.Commerce.ProductName(),
+                        Price = faker.Random.Decimal(10000, 1000000),
+                        CategoryId = category.Id
+                    };
+
+                    context.Products.Add(product);
+                }
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
