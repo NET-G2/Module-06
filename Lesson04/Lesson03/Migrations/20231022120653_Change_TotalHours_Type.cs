3ea41f7e8260cabac27248cd8356ccbc55d93759
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2023-10-28 14:18:48 +0200

    class exercises

---
+++ Lesson04/Lesson03/Migrations/20231022120653_Change_TotalHours_Type.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lesson03.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_TotalHours_Type : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalHours",
+                table: "Subject",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(3)",
+                oldPrecision: 3,
+                oldScale: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalHours",
+                table: "Subject",
+                type: "float(3)",
+                precision: 3,
+                scale: 1,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
