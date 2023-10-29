1aaddffe802cdb44694e635f858b7ecdaf7b7d9c
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-10-29 10:21:49 +0500

    update project

---
+++ Lesson03/Lesson03/Models/Subject.cs
@@ -0,0 +1,21 @@
+﻿namespace Lesson03.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfModules { get; set; }
+        public int TotalHours { get; set; }
+
+        public virtual ICollection<CourseGroup> Courses { get; set; }
+        public virtual ICollection<Assignment> Assignments { get; set; }
+
+        public Subject() 
+        {
+            Assignments = new List<Assignment>();
+            Courses = new List<CourseGroup>();
+        }
+    }
+}
