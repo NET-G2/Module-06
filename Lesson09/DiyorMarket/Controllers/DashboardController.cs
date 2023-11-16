7e80f66641cdb8bf7812200d33663e46c7d7482c
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2023-11-16 12:17:46 +0100

    update UI dashboard

--- Lesson09/DiyorMarket/Controllers/DashboardController.cs
+++ Lesson09/DiyorMarket/Controllers/DashboardController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiyorMarket.Domain.Entities;
+using DiyorMarket.Domain.Interfaces.Repositories;
+using DiyorMarket.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiyorMarket.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly DiyorMarketDbContext _context;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public DashboardController(ICategoryRepository categoryRepository, DiyorMarketDbContext context)
+        {
+            _categoryRepository = categoryRepository;
+            _context = context;
+        }
 
         public IActionResult Index()
         {
+            var productCategories = _context.ProductCategories.ToList();
+
+            var result = from c in _context.Categories
+                         join pc in _context.ProductCategories on c.Id equals pc.CategoryId
+                         join si in _context.SaleItems on pc.ProductId equals si.ProductId
+                         group si by c.Name into groupedCategories
+                         select new
+                         {
+                             CategoryName = groupedCategories.Key,
+                             SalesCount = groupedCategories.Count()
+                         };
+
+            ViewBag.Categories = result;
+
             return View();
         }
     }
@@ -19,14 +44,17 @@ namespace DiyorMarket.Controllers
         public string FitterValue { get; set; }
         public string[] FitterData { get; set; }
 
+        public string DocumentValue { get; set; }
+        public string[] DocumentData { get; set; }
+
         public ComboBoxFor()
         {
             CategoryData = new string[]
             {
                 "Все",
-                "Игрушки",
-                "Сладости",
-                "Напитки"
+                "Законченные",
+                "В процессе",
+                "Проблемы"
             };
         }
     }
